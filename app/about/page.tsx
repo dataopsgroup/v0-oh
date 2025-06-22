d62316159aa6e37d24b086e99eb2aec6799a8c07
DataOps Group <GEOFF@dataopsgroup.com>
2025-06-22 14:29:03 -0500

    Update page.tsx
    type PageLayoutProps = {
      children?: React.ReactNode;  // Add the ? to make it optional
    }

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,3 @@
-import PageLayout from "@/components/layout/PageLayout"
-
-export default function AboutPage() {
-  return <PageLayout>{/* All existing about page content goes here */}</PageLayout>
+type PageLayoutProps = {
+  children?: React.ReactNode;  // Add the ? to make it optional
 }
