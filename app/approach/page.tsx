04fe556a6af2d77c3baf1b832df28d28f1dd0e1a
DataOps Group <GEOFF@dataopsgroup.com>
2025-06-22 17:33:56 -0500

    Update page.tsx

--- app/approach/page.tsx
+++ app/approach/page.tsx
@@ -1,4 +1,5 @@
-export default function ApproachPage() {
+import PageLayout from "@/components/layout/PageLayout"
+  export default function ApproachPage() {
   return (
     <PageLayout>
       <div className="min-h-screen p-8">
