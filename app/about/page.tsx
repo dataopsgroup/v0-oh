915e6f42883cfce711f83ffe6f53d3e0a7832328
DataOps Group <GEOFF@dataopsgroup.com>
2025-06-22 17:11:19 -0500

    Update page.tsx
    import PageLayout from "@/components/layout/PageLayout"
    export default function AboutPage() {
      return <PageLayout>{/* All existing about page content goes here */}</PageLayout>
    }

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,4 @@
 import PageLayout from "@/components/layout/PageLayout"
-
 export default function AboutPage() {
-  return (
-    <PageLayout>
-      <div className="min-h-screen bg-gradient-to-br from-gray-50 to-white">{/* Rest of the existing content */}</div>
-    </PageLayout>
-  )
+  return <PageLayout>{/* All existing about page content goes here */}</PageLayout>
 }
